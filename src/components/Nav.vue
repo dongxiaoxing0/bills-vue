c350eccfb6c7c9ee5db8fd41e346ccce897e0132
dongxiaoxing <dongxiaoxing0@gmail.com>
2020-11-19 23:24:13 +0800

    创建Icon组件，引入SVG

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="nav">
-      <router-link to="/money">记账</router-link>
+      <router-link to="/money">
+      记账
+        <Icon name="money"></Icon>
+      </router-link>
       |
-      <router-link to="/labels">标签</router-link>
+      <router-link to="/labels">
+      标签
+        <Icon name="label"></Icon>
+      </router-link>
       |
-      <router-link to="/statistics">统计</router-link>
+      <router-link to="/statistics">
+      统计
+        <Icon name="statistics"></Icon>
+      </router-link>
     </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
 
     export default {
         name: 'Nav'
