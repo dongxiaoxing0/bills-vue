ce39d0ec598834db2bc99a502ff2bc704546622b
dongxiaoxing <dongxiaoxing0@gmail.com>
2020-11-22 07:04:11 +0800

    添加一个TS组件，实现支出和收入切换

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,14 +1,25 @@
 <template>
     <ul class="types">
-        <li class="selected">支出</li>
-        <li>收入</li>
+        <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+        <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
 </template>
 
 <script lang="ts">
-    export default {
-        name:'Types',
+  import Vue from 'vue'
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Types extends Vue{
+    type = '-'//'-'表示支出，'+'表示收入
+    selectType(type: string){//type只能是'-'和'+'中的一个
+      if(type !== '-' && type !== '+'){
+        throw new Error('type is unknown')
+      }else{
+        this.type = type
+      }
     }
+  }
 </script>
 
 <style lang="scss" scoped>
